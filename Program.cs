e906f4e7e9f56ade1ad1bd6c4dd13969d58d114e
Shouvik <sd3059@srmist.edu.in>
2024-01-30 07:58:45 +0530

    [Shouvik] Update : Ability to add multiple person to address book

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -85,50 +86,55 @@ namespace ABS
 
     class AddressBookMain
     {
-        Contact contact;
+        List<Contact> con;
         public AddressBookMain()
         {
-            contact = new Contact();
+            con = new List<Contact>();
         }
 
         public void AddContact()
         {
+            Contact contacts = new Contact();
             Console.WriteLine("First Name");
             string firstname = Console.ReadLine();
-            contact.set_firstname(firstname);
+            contacts.set_firstname(firstname);
             Console.WriteLine("Last Name");
             string lastname = Console.ReadLine();
-            contact.set_lastname(lastname);
+            contacts.set_lastname(lastname);
             Console.WriteLine("Address");
             string address = Console.ReadLine();
-            contact.set_address(address);
+            contacts.set_address(address);
             Console.WriteLine("City");
             string city = Console.ReadLine();
-            contact.set_city(city);
+            contacts.set_city(city);
             Console.WriteLine("State");
             string state = Console.ReadLine();
-            contact.set_state(state);
+            contacts.set_state(state);
             Console.WriteLine("ZipCode");
             int zipcode = Convert.ToInt32(Console.ReadLine());
-            contact.set_zipcode(zipcode);
+            contacts.set_zipcode(zipcode);
             Console.WriteLine("Phone Number");
             long phonenumber = Convert.ToInt64(Console.ReadLine());
-            contact.set_phonenumber(phonenumber);
+            contacts.set_phonenumber(phonenumber);
             Console.WriteLine("Email");
             string email = Console.ReadLine();
-            contact.set_email(email);
-
+            contacts.set_email(email);
+            con.Add(contacts);
         }
         public void display()
         {
-            Console.WriteLine($"First Name : {contact.get_firstname()}");
-            Console.WriteLine($"Last Name : {contact.get_lastname()}");
-            Console.WriteLine($"Address : {contact.get_address()}");
-            Console.WriteLine($"City : {contact.get_City()}");
-            Console.WriteLine($"State : {contact.get_State()}");
-            Console.WriteLine($"Zip Code : {contact.get_ZipCode()}");
-            Console.WriteLine($"Phone Number : {contact.get_PhoneNumber()}");
-            Console.WriteLine($"Email : {contact.get_Email()}");
+            for (int i = 0; i < con.Count; i++)
+            {
+                Console.WriteLine($"\nContact : {i+1}\n");
+                Console.WriteLine($"First Name : {con[i].get_firstname()}");
+                Console.WriteLine($"Last Name : {con[i].get_lastname()}");
+                Console.WriteLine($"Address : {con[i].get_address()}");
+                Console.WriteLine($"City : {con[i].get_City()}");
+                Console.WriteLine($"State : {con[i].get_State()}");
+                Console.WriteLine($"Zip Code : {con[i].get_ZipCode()}");
+                Console.WriteLine($"Phone Number : {con[i].get_PhoneNumber()}");
+                Console.WriteLine($"Email : {con[i].get_Email()}");
+            }
         }
 
         public void Edit(string Email)
@@ -137,52 +143,68 @@ namespace ABS
             string str;
             long phno;
             int n;
-            do
+            for (int i = 0; i < con.Count; i++)
             {
-                display();
-                Console.WriteLine();
-                Console.WriteLine("Enter which portion to be edited ..");
-                Console.WriteLine("1. FirstName\n2. Last Name\n3. Address\n4. City\n5. State\n6. ZipCode\n7. Phone Number\n8. Email\n9. Done");
-                option = Convert.ToInt32(Console.ReadLine());
-                switch(option)
-                {
-                    case 1: Console.WriteLine("Enter the First New Name : ");
-                            str=Console.ReadLine();
-                            contact.set_firstname(str);
-                            break;
-                    case 2: Console.WriteLine("Enter the Last New Name : ");
-                            str=Console.ReadLine();
-                            contact.set_lastname(str);
-                            break;
-                    case 3: Console.WriteLine("Enter the New Address : ");
-                            str=Console.ReadLine();
-                            contact.set_address(str);
-                            break;
-                    case 4: Console.WriteLine("Enter the New City : ");
-                            str=Console.ReadLine();
-                            contact.set_city(str);
-                            break;
-                    case 5: Console.WriteLine("Enter the New State : ");
-                            str=Console.ReadLine();
-                            contact.set_state(str);
-                            break;
-                    case 6: Console.WriteLine("Enter the New ZipCode : ");
-                            n = Convert.ToInt32(Console.ReadLine());
-                            contact.set_zipcode(n);
-                            break;
-                    case 7: Console.WriteLine("Enter the New Phone Number : ");
-                            phno = Convert.ToInt64(Console.ReadLine());
-                            contact.set_phonenumber(phno);
-                            break;
-                    case 8: Console.WriteLine("Enter the New Email : ");
-                            str=Console.ReadLine();
-                            contact.set_email(str);
-                            break;
-                    case 9: Console.WriteLine("All Edits done..");
-                            break;
-                }
-            }while (option!=9);
+                if (con[i].get_Email()==Email)
+                    {
+                        do
+                        {
+                            Console.WriteLine();
+                        Console.WriteLine("Enter which portion to be edited ..");
+                        Console.WriteLine("1. FirstName\n2. Last Name\n3. Address\n4. City\n5. State\n6. ZipCode\n7. Phone Number\n8. Email\n9. Done");
+                        option = Convert.ToInt32(Console.ReadLine());
+                        switch (option)
+                        {
+                            case 1:
+                                Console.WriteLine("Enter the First New Name : ");
+                                str = Console.ReadLine();
+                                con[i].set_firstname(str);
+                                break;
+                            case 2:
+                                Console.WriteLine("Enter the Last New Name : ");
+                                str = Console.ReadLine();
+                                con[i].set_lastname(str);
+                                break;
+                            case 3:
+                                Console.WriteLine("Enter the New Address : ");
+                                str = Console.ReadLine();
+                                con[i].set_address(str);
+                                break;
+                            case 4:
+                                Console.WriteLine("Enter the New City : ");
+                                str = Console.ReadLine();
+                                con[i].set_city(str);
+                                break;
+                            case 5:
+                                Console.WriteLine("Enter the New State : ");
+                                str = Console.ReadLine();
+                                con[i].set_state(str);
+                                break;
+                            case 6:
+                                Console.WriteLine("Enter the New ZipCode : ");
+                                n = Convert.ToInt32(Console.ReadLine());
+                                con[i].set_zipcode(n);
+                                break;
+                            case 7:
+                                Console.WriteLine("Enter the New Phone Number : ");
+                                phno = Convert.ToInt64(Console.ReadLine());
+                                con[i].set_phonenumber(phno);
+                                break;
+                            case 8:
+                                Console.WriteLine("Enter the New Email : ");
+                                str = Console.ReadLine();
+                                con[i].set_email(str);
+                                break;
+                            case 9:
+                                Console.WriteLine("All Edits done..");
+                                break;
+                        }
+                    } while (option != 9);
+                }    
+            }
         }
+        
+
         public void DisplayMessage()
         {
             Console.WriteLine("Welcome to Address Book Program");
@@ -205,9 +227,12 @@ namespace ABS
             myobject.AddContact();
             myobject.display();
             Console.WriteLine();
-            Console.WriteLine("Enter the email : ");
+            myobject.display();
+            Console.WriteLine("\nEnter the email : ");
             string email = Console.ReadLine();
             myobject.Edit(email);
+
+            
         }
     }
 }
